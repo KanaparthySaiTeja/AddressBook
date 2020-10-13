1628668af88883c6565f4ae3747dfd3fb907a4dc
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-13 19:08:23 +0530

    edit contact

--- AddressBook/src/main/java/linecomp/AddressBook/Address.java
+++ AddressBook/src/main/java/linecomp/AddressBook/Address.java
@@ -1,17 +1,52 @@
 package linecomp.AddressBook;
 
-
-/**
- * Hello world!
- *
- */
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Address {
-    public static void main(String[] args) {
-		System.out.println("welcome");
-		System.out.println("Contact created");
+	private List<Contact> arrayList = new CopyOnWriteArrayList<Contact>();
+
+	public void setArrayList(List<Contact> obj) {
+		arrayList = obj;
+	}
+
+	public List<Contact> getArrayList() {
+		return arrayList;
+	}
+
+	public boolean checkDuplicate(Contact obj) {
+		return arrayList.stream().anyMatch(duplicate -> duplicate.equals(obj));
+	}
+
+	public void addContact(Contact contactObj) {
+			arrayList.add(contactObj);
+	}
+	public List<Contact> viewAllContacts() {
+		return arrayList;
+	}
+
+	public boolean editContactGivenName(String firstName, String lastName) {
+		boolean f = false;
+		Scanner sc = new Scanner(System.in);
+		for (Contact obj2 : arrayList) {
+			if (obj2.getFirstName().equalsIgnoreCase(firstName) && obj2.getLastName().equalsIgnoreCase(lastName)) {
+				f = true;
+				System.out.println("Enter Address");
+				obj2.setAddress(sc.nextLine());
+				System.out.println("Enter city");
+				obj2.setCity(sc.nextLine());
+				System.out.println("Enter state");
+				obj2.setState(sc.nextLine());
+				System.out.println("Enter zip code");
+				obj2.setZip(Long.parseLong(sc.nextLine()));
+				System.out.println("Enter the Phone No.: ");
+				obj2.setPhoneNumber(Long.parseLong(sc.nextLine()));
+				System.out.println("Enter the Email: ");
+				obj2.setEmailId(sc.nextLine());
+				break;
+			}
+		}
+		return f;
 	}
 }
-
-
-
