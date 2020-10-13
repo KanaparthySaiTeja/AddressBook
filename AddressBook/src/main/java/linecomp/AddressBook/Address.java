97a1f9ed4515615bd1086f9e6527f376e899ceca
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-13 18:48:50 +0530

    welcome

--- AddressBook/src/main/java/linecomp/AddressBook/Address.java
+++ AddressBook/src/main/java/linecomp/AddressBook/Address.java
@@ -1,9 +1,17 @@
 package linecomp.AddressBook;
 
-import java.util.Scanner;
-public class AddressBook {
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Address {
     public static void main(String[] args) {
 		System.out.println("welcome");
 		System.out.println("Contact created");
 	}
 }
+
+
+
